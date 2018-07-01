aa9affe4076799ea0cd589471fe3b33c6e3f0158
LT0143 <1162710759@qq.com>
2018-07-01 20:16:14 +0800

    添加工具类

--- CopyGameFramework/Utility/Converter.cs
+++ CopyGameFramework/Utility/Converter.cs
@@ -16,6 +16,89 @@ namespace CopyGameFramework
         /// </summary>
         public static class Converter
         {
+            private const float InchesToCentimeters = 2.54f;//1 inch英尺 =2.54cm
+            private const float CentimetersToInches = 1f / InchesToCentimeters; // 1 cm = 0.3937 inches
+
+            /// <summary>
+            /// 获取数据在此计算机结构中存储时的字节顺序。
+            /// </summary>
+            public static bool IsLittleEndian
+            {
+                get
+                {
+                    return BitConverter.IsLittleEndian;
+                }
+            }
+
+            /// <summary>
+            /// 获取或设置屏幕每英寸点数。
+            /// </summary>
+            public static float ScreenDpi
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// 将像素转换为厘米。
+            /// </summary>
+            /// <param name="pixels">像素。</param>
+            /// <returns>厘米。</returns>
+            public static float GetCentimetersFromPixels(float pixels)
+            {
+                if (ScreenDpi <= 0)
+                {
+                    throw new GameFrameworkException("You must set screen DPI first.");
+                }
+
+                return InchesToCentimeters * pixels / ScreenDpi;
+            }
+
+            /// <summary>
+            /// 将厘米转换为像素。
+            /// </summary>
+            /// <param name="centimeters">厘米。</param>
+            /// <returns>像素。</returns>
+            public static float GetPixelsFromCentimeters(float centimeters)
+            {
+                if (ScreenDpi <= 0)
+                {
+                    throw new GameFrameworkException("You must set screen DPI first.");
+                }
+
+                return    centimeters / CentimetersToInches * ScreenDpi;
+            }
+
+            /// <summary>
+            /// 将像素转换为英寸。
+            /// </summary>
+            /// <param name="pixels">像素。</param>
+            /// <returns>英寸。</returns>
+            public static float GetInchesFromPixels(float pixels)
+            {
+                if (ScreenDpi <= 0)
+                {
+                    throw new GameFrameworkException("You must set screen DPI first.");
+                }
+
+                return pixels / ScreenDpi;
+            }
+
+            /// <summary>
+            /// 将英寸转换为像素。
+            /// </summary>
+            /// <param name="inches">英寸。</param>
+            /// <returns>像素。</returns>
+            public static float GetPixelsFromInches(float inches)
+            {
+                if (ScreenDpi <= 0)
+                {
+                    throw new GameFrameworkException("You must set screen DPI first.");
+                }
+
+                return inches * ScreenDpi;
+            }
+
         }
     }
 }
